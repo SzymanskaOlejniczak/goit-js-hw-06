2ccd55fdf7afb0a2d83b2f6965415b55620763a4
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-09-18 17:59:52 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
-document.querySelectorAll("ul")
-console.log(ul)
+const numberOfCategories = document.querySelectorAll("li.item");
+console.log(`Number of Categories:${numberOfCategories.length}`);
 
+const animal = numberOfCategories[0].firstElementChild;
+const animal2 = numberOfCategories[0].querySelector("h2")
+console.log(animal2);
+
+const listAnimals =  numberOfCategories[0].lastElementChild.querySelectorAll("li");
+console.log(`Elements: ${listAnimals.length}`);
