4f8fb55adeeac9cd6613fea2655ca7e7c01f3e96
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-09-21 10:55:57 +0200

    Create task-09.js

---
+++ js/task-09.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+const myBody = document.querySelector("body");
+
+const changeColorButton = document.querySelector(".change-color");
+
+const changeColorSpan = document.querySelector(".color");
+const changeRandomColor = ()=> {
+  myBody.style.backgroundColor = `${getRandomHexColor()}`;
+  changeColorSpan.textContent = `${getRandomHexColor()}`;
+}
+changeColorButton.addEventListener("click", changeRandomColor);
